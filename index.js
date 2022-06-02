3391f4a62443cccec7973ddb5ec71dccd3629efa
GordonGichini <gordongichini1@gmail.com>
2022-06-02 17:35:08 +0300

    commit tree 321aa12720adf747cb2f8d3b8652be9bfb43da10

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string){
+    return string.toUpperCase();
+}
+shout(hello);
+
+function whisper(string){
+    return string.toLowerCase();
+}
+whisper(HELLO);
+function logShout(string){
+    console.log(string.toUpperCase());
+} 
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+    if (string.toLowerCase()===string) {return "I can't hear you!"}
+    if (string.toUpperCase()===string) {return "YES INDEED!"}
+    if (string===string) {return "I would love to!"}
+}
+sayHiToHeadphonedRoommate(hello);
+sayHiToHeadphonedRoommate(HELLO);
+sayHiToHeadphonedRoommate(LetsHaveDinnerTogether);
